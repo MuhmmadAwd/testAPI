4829228360c0b9a410ad0ea797e27a0e2f3c739b
mohmed2000 <noza25588@gmail.com>
2021-06-06 10:02:16 +0300

    init libraries
    #1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
