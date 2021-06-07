5b2229dd3d0da581e2b296de1fcacd5c6e3a850a
mohmed2000 <noza25588@gmail.com>
2021-06-07 21:37:11 +0300

    add-to-cart
    #4

--- src/Reducer/Reducer.js
+++ src/Reducer/Reducer.js
@@ -21,18 +21,10 @@ const Reducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cart: inCart
-          ? state.cart.map((item) =>
-              item.id === action.payload.id
-                ? { ...item, qty: item.qty + 1 }
-                : item
-            )
-          : [...state.cart, { ...item, qty: 1 }],
+          ? state.cart.map((item) => item.id === action.payload.id)
+          : [...state.cart, { ...item }],
       };
-    case actionTypes.REMOVE_FROM_CART:
-      return {
-        ...state,
-        cart: state.cart.filter((item) => item.id !== action.payload.id),
-      };
+
     case actionTypes.LOAD_CURRENT_ITEM:
       return {
         ...state,
